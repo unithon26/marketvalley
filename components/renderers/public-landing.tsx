9b24e4c3ef241b93c9518ee20472fafc86474169
ghdtjdwn <seongjuice999@gmail.com>
2026-08-26 04:25:30 +0900

    test: align end-to-end report expectations

--- components/renderers/public-landing.tsx
+++ components/renderers/public-landing.tsx
@@ -232,6 +232,9 @@ export function PublicLanding({
   }, [renderTurnstile]);
 
   useEffect(() => {
+    if (!/^[0-9a-f]{8}-[0-9a-f]{4}-[1-8][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/iu.test(campaignId)) {
+      return;
+    }
     const controller = new AbortController();
     void fetch("/api/analytics/visits", {
       method: "POST",
